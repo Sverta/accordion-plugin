2fd4c877a9afd01d9d08b5746508ec15b53c4b4b
Sviatlana Baidak <sbaidak@scnsoft.com>
2016-07-19 10:43:00 +0300

    commit

--- js/plugin.js
+++ js/plugin.js
@@ -1,15 +1,3 @@
-/*(function($) {
-    jQuery.fn.accordionBlock = function() {
-
-        $(this).find('ul').hide();
-        $(this).find('a').click(function() {
-            if(false == $(this).next().is(':visible')) {
-                $('ul').find('ul').slideUp("normal");
-            }
-            $(this).next().slideToggle("normal");
-        });
-    };
-})(jQuery);*/
 
 (function($) {
     jQuery.fn.accordionBlock = function() {
